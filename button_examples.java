4115a21b550b40fbe72b496a5ee323b15aad7e3f
SeshMelchor34 <eltechau20@gmail.com>
2025-01-06 17:09:53 -0500

    Update button_examples.java

--- button_examples.java
+++ button_examples.java
@@ -1,12 +1,76 @@
- class library {
-
-    //Example for how to create a button and set an action command for it
-   /* btnClick=newJButton("Send text!");
-   btnClick.setActionCommand("english")
-   ;btnItalian=newJButton("Diff text!")
-   ;btnItalian.setActionCommand("italian");*/
+ class library {   
+/*Example for how to create a button and set an action command for it
+   btnClick=newJButton("Send text!");
+   btnClick.setActionCommand("english");
+   btnItalian=newJButton("Diff text!");
+   btnItalian.setActionCommand("italian");
   
-   /*  instanceButtonHandler onClick =newButtonHandler();
-    btnClick.addActionListener(onClick); */
+   instanceButtonHandler onClick =newButtonHandler();
+    btnClick.addActionListener(onClick);
+    end of simple buton example 
+*/
+    
+/* Fig. 12.10: ButtonTest.java
+
+Creating JButtons.
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class ButtonTest extends JFrame {
+   private JButton plainButton, fancyButton;
+
+   public ButtonTest()
+   {
+      super( "Testing Buttons" );
+
+      Container c = getContentPane();
+      c.setLayout( new FlowLayout() );
+
+      // create buttons
+      plainButton = new JButton( "Plain Button" );
+      c.add( plainButton );
+
+      Icon bug1 = new ImageIcon( "bug1.gif" );
+      Icon bug2 = new ImageIcon( "bug2.gif" );
+      fancyButton = new JButton( "Fancy Button", bug1 );
+      fancyButton.setRolloverIcon( bug2 );
+      c.add( fancyButton );
+
+      // create an instance of inner class ButtonHandler
+      // to use for button event handling 
+      ButtonHandler handler = new ButtonHandler();
+      fancyButton.addActionListener( handler );
+      plainButton.addActionListener( handler );
+
+      setSize( 275, 100 );
+      show();
+   }
+
+   public static void main( String args[] )
+   { 
+      ButtonTest app = new ButtonTest();
+
+      app.addWindowListener(
+         new WindowAdapter() {
+            public void windowClosing( WindowEvent e )
+            {
+               System.exit( 0 );
+            }
+         }
+      );
+   }
+
+   // inner class for button event handling
+   private class ButtonHandler implements ActionListener {
+      public void actionPerformed( ActionEvent e )
+      {
+         JOptionPane.showMessageDialog( null,
+            "You pressed: " + e.getActionCommand() );
+      }
+   }
+}
+ end of button test
+*/
     
 }
