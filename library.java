3ba696b36ce72fc3735a33bef4c33e6e7acfc297
SeshMelchor34 <eltechau20@gmail.com>
2025-01-06 16:59:29 -0500

    Create library.java

---
+++ library.java
@@ -0,0 +1,12 @@
+ class library {
+
+    //Example for how to create a button and set an action command for it
+   /* btnClick=newJButton("Send text!");
+   btnClick.setActionCommand("english")
+   ;btnItalian=newJButton("Diff text!")
+   ;btnItalian.setActionCommand("italian");*/
+  
+   /*  instanceButtonHandler onClick =newButtonHandler();
+    btnClick.addActionListener(onClick); */
+    
+}
