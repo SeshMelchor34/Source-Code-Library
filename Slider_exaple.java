d523afb3f0684b3dc81c8cc712c7355e102cdf1f
SeshMelchor34 <155834265+SeshMelchor34@users.noreply.github.com>
2025-01-07 09:12:29 -0500

    Create Slider_exaple.java

---
+++ Slider_exaple.java
@@ -0,0 +1,46 @@
+{
+   private JSlider diameter;
+   private OvalPanel myPanel;
+
+   public SliderDemo() 
+   {
+      super( "Slider Demo" );
+
+      myPanel = new OvalPanel();
+      myPanel.setBackground( Color.yellow );
+
+      diameter = new JSlider( SwingConstants.HORIZONTAL,
+                              0, 200, 10 );
+      diameter.setMajorTickSpacing( 10 );
+      diameter.setPaintTicks( true );
+      diameter.addChangeListener(
+         new ChangeListener() {
+            public void stateChanged( ChangeEvent e )
+            {
+               myPanel.setDiameter( diameter.getValue() );
+            }
+         }
+      );
+
+      Container c = getContentPane();
+      c.add( diameter, BorderLayout.SOUTH );
+      c.add( myPanel, BorderLayout.CENTER );
+
+      setSize( 220, 270 );
+      show();
+   }
+
+   public static void main( String args[] )
+   {
+      SliderDemo app = new SliderDemo();
+
+      app.addWindowListener(
+         new WindowAdapter() {
+            public void windowClosing( WindowEvent e )
+            {
+               System.exit( 0 );
+            }
+         }
+      );
+   }
+}
