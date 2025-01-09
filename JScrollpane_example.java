9685a7b83c961d90fb70ba9c89c954bbc6b21229
SeshMelchor34 <155834265+SeshMelchor34@users.noreply.github.com>
2025-01-09 09:15:25 -0500

    Update JScrollpane_example.java

--- JScrollpane_example.java
+++ JScrollpane_example.java
@@ -18,3 +18,35 @@ ImageIcon david = createImageIcon("images/youngdad.jpeg");
   pictureScrollPane.setColumnHeaderView(columnView);
   pictureScrollPane.setRowHeaderView(rowView);
 
+//The ScrollDemo program establishes the scroll pane's client when creating the scroll pane:
+//Where the member variables are declared:
+  private ScrollablePicture picture;
+//Where the GUI is created:
+  picture = new ScrollablePicture( ... );
+  JScrollPane pictureScrollPane = new JScrollPane(picture);
+
+//Constructors to set the policies for the scroll pane
+JScrollPane(Component, int, int)
+JScrollPane(int, int)
+
+ //Create the corners.
+JPanel buttonCorner = new JPanel(); //use FlowLayout
+isMetric = new JToggleButton("cm", true);
+isMetric.setFont(new Font("SansSerif", Font.PLAIN, 11));
+isMetric.setMargin(new Insets(2,2,2,2));
+isMetric.addItemListener(this);
+buttonCorner.add(isMetric);
+...
+//Set the corners.
+pictureScrollPane.setCorner(JScrollPane.UPPER_LEFT_CORNER,buttonCorner);
+pictureScrollPane.setCorner(JScrollPane.LOWER_LEFT_CORNER,new Corner());
+pictureScrollPane.setCorner(JScrollPane.UPPER_RIGHT_CORNER,new Corner());
+
+if (changed) {
+//Update client's preferred size because
+//the area taken up by the graphics has
+//gotten larger or smaller (if cleared).
+drawingArea.setPreferredSize(/* the new size */);
+//Let the scroll pane know to update itself
+//and its scroll bars.
+drawingArea.revalidate();
